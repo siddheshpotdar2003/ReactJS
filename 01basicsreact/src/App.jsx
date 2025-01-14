9fedb9993fb61703efa6b8d312a92ac2e2f31939
siddheshpotdar2003 <potdarsiddhesh2003@gmail.com>
2025-01-14 11:22:02 +0530

    feat: Counter

--- 01basicsreact/src/App.jsx
+++ 01basicsreact/src/App.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <>
       <h1>Hello React</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={() => setCounter((prev) => prev + 1)}>Increment</button>
+      <button onClick={() => setCounter((prev) => prev - 1)}>Decrement</button>
     </>
   );
 }
