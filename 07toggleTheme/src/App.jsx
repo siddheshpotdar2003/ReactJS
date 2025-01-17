53253416c36840bd8cbc64e4fb08de816301b03a
siddheshpotdar2003 <potdarsiddhesh2003@gmail.com>
2025-01-17 22:44:42 +0530

    07toggleTheme: Using Context

--- 07toggleTheme/src/App.jsx
+++ 07toggleTheme/src/App.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import Button from "./components/Button";
 import Card from "./components/Card";
+import { ThemeProvider } from "./context/theme";
+import { useEffect } from "react";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("dark", "light");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-4 bg-gray-100 dark:bg-gray-900 transition-colors duration-300">
-      <Button />
-      <Card />
-    </div>
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex h-screen flex-col items-center justify-center gap-4 bg-gray-100 dark:bg-gray-900 transition-colors duration-300">
+        <Button />
+        <Card />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- 07toggleTheme/src/components/Button.jsx
+++ 07toggleTheme/src/components/Button.jsx
@@ -1,30 +1,20 @@
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import useTheme from "../context/theme";
 
-const Button = (e) => {
-  const [darkModeStatus, setDarkModeStatus] = useState(false);
+const Button = () => {
+  const { themeMode, darkTheme, lightTheme } = useTheme();
+
   const toggleDarkMode = (e) => {
-    setDarkModeStatus(e.target.checked);
+    const isDarkMode = e.target.checked;
+    isDarkMode ? darkTheme() : lightTheme();
   };
-
-  useEffect(() => {
-    document.querySelector("html").classList.remove("dark", "light");
-    if (darkModeStatus) {
-      document.querySelector("html").classList.add("dark");
-    } else {
-      document.querySelector("html").classList.add("light");
-    }
-    console.log(darkModeStatus);
-    console.log(document.querySelector("html").classList);
-  }, [darkModeStatus]);
   return (
     <label className="relative inline-flex items-center cursor-pointer">
       <input
         type="checkbox"
         className="sr-only peer"
-        checked={darkModeStatus}
         onChange={toggleDarkMode}
+        checked={themeMode == "dark"}
       />
       <div className="w-14 h-8 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-6 peer-checked:after:border-white after:content-[''] after:absolute after:top-1 after:left-1 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:after:bg-gray-300 peer-checked:bg-blue-600"></div>
       <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
