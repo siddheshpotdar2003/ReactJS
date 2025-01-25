067d7d9f89b148cbe41b7288a9d95ab7c5eff0a8
siddheshpotdar2003 <potdarsiddhesh2003@gmail.com>
2025-01-25 14:06:21 +0530

    10MegaProject: Signupt & Login From

--- 10MegaProject/src/components/index.js
+++ 10MegaProject/src/components/index.js
@@ -3,5 +3,19 @@ import Footer from "./Footer.jsx";
 import Logo from "./Logo.jsx";
 import Container from "./Container.jsx";
 import LogoutBtn from "./LogoutBtn.jsx";
+import Login from "./Login.jsx";
+import Signup from "./Signup.jsx";
+import PostCard from "./PostCard.jsx";
+import Protected from "./AuthLayout.jsx";
 
-export { Header, Footer, Logo, Container, LogoutBtn };
+export {
+  Header,
+  Footer,
+  Logo,
+  Container,
+  LogoutBtn,
+  Login,
+  Signup,
+  PostCard,
+  Protected,
+};
